9f73ed304613142432053e231634e2361f33e92a
rustem <3434@.ru>
2023-10-20 18:49:56 +0500

    t

---
+++ app.js
@@ -0,0 +1,78 @@
+let btn = document.querySelector('.btn'),
+    input = document.querySelector('.input'),
+    timeOut = document.querySelector('.time'),
+    gameBox = document.querySelector('.game__block'),
+    time = 0,
+    score = 0,
+    interval = 0;
+    
+btn.addEventListener('click', (event) => {
+    event.preventDefault()
+    if(input.value > 4) {
+        time = input.value
+        input.value = ''
+        score = 0
+        clearInterval(interval)
+        start()
+        let result = document.querySelector('.result')
+        if(result) {
+            result.style.display = 'none'
+        }
+    }
+})
+
+gameBox.addEventListener('click', (event) => {
+    if(event.target.classList.contains('ball')) {
+        score++
+        event.target.remove()
+        createBall()
+    }
+})
+
+
+
+
+function start() {
+    btn.disabled = true
+    timeOut.innerHTML = time
+    interval = setInterval(() => {
+        decrease()
+    }, 1000);
+    createBall()
+}
+
+function decrease() {
+    if(time == 0) {
+        endGame()
+    }else {
+        let currentTime = --time
+        timeOut.innerHTML = currentTime
+    }
+}
+
+function endGame() {
+    gameBox.innerHTML = `<h2 class="result">Вы набрали: ${score} очков</h2>`
+    btn.disabled = false
+}
+
+function createBall() {
+    let ball = document.createElement('div')
+    ball.classList.add('ball')
+    let size = 40
+    let coor = gameBox.getBoundingClientRect()
+    let x = random(0, coor.width - size)
+    let y = random(0, coor.height - size)
+    
+    ball.style.width = size + 'px' 
+    ball.style.height = size + 'px'
+    ball.style.background = 'red'
+    ball.style.top = y + 'px'
+    ball.style.left = x + 'px'
+    
+    gameBox.append(ball)
+}
+
+function random(min,max){
+    return Math.floor(Math.random() * (max + 1 - min) + min)
+}
+ 
